3ad46571b61419728e385ef1003e290ebfdda4c5
wangw571 <wang_w571@hotmail.com>
2015-09-16 15:26:06 -0400

    Update Wheel.cs

--- Wheel.cs
+++ Wheel.cs
@@ -1,85 +1,96 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.IO;
-using UnityEngine;
-using spaar.ModLoader;
-using System.Runtime.Serialization.Formatters.Binary;
-using TheGuysYouDespise;
+    using System;
+    using System.Collections.Generic;
+    using spaar.ModLoader;
+    using TheGuysYouDespise;
+    using UnityEngine;
 
-namespace Wheel
-{
-    public class BesiegeModLoader : Mod{
-        public override string Name { get { return "wheelMod"; } }
-        public override string DisplayName { get { return "wheel Mod"; } }
-        public override string BesiegeVersion { get { return "v0.11"; } }
-        public override string Author { get { return "覅是 & tony1020"; } }
-        public override Version Version { get { return new Version(100, 100); } }
-        public override bool CanBeUnloaded { get { return true; } }
-        public bool enabled;
-
-        public GameObject temp;
-
-        VisualOffset offset = new VisualOffset(Vector3.zero, Vector3.zero, Vector3.zero);
-
-        Icon icon = new Icon(0.2f, new Vector3(-0.1f, -0.1f, 1f), new Vector3(-90f, 45f, 0f));
-
-        BlockProperties blockProp = new BlockProperties().Slider("Speed",0f,5f,1f).Key1("Forward","up").Key2("backward","down");
-        List<ColliderComposite> points2 = new List<ColliderComposite> {new ColliderComposite("wheel.obj", new Vector3(0,0,0),new Vector3(0,0,0))
-    };
-        WheelCollider points3 = new WheelCollider();//Not using
-        
-        
-        Type[] scripts = new Type[] { typeof(wheelMod) };
-
-        List<NeededResource> neededResources = new List<NeededResource> { /*new NeededResource(ResourceType.Audio, "Wheel.ogg")*/ };
-
-
-
-        public override void OnLoad()
-        {
-            //GameObject temp = new GameObject ();
-            List<AddingPoint> points = new List<AddingPoint> { (AddingPoint)new BasePoint(true, true).Motionable(false/*旋转*/, false,false).SetStickyRadius(0.5f),
-                                                    /*new AddingPoint(new Vector3(0f, 0f, 0.5f), new Vector3(0f, 0f, 0f)),
-                                                    new AddingPoint(new Vector3(0f, 0f, 0.5f), new Vector3(0f, 0f, 90f)),
-                                                    new AddingPoint(new Vector3(0f, 0f, 0.5f), new Vector3(0f, 0f, 180f)),
-                                                    new AddingPoint(new Vector3(0f, 0f, 0.5f), new Vector3(0f, 0f, 270f)),
-                                                    //Top:
-                                                    new AddingPoint(new Vector3(0f, 0f, 0.5f), new Vector3(-90f, 0f, 0f), true)*/ };
-            BlockLoader.AddModBlock(501, "wheel.obj", "whell_map.png", "Morden Wheel", icon, blockProp, points2, neededResources, 2f, offset, scripts, false, points);
-            //temp.AddComponent<ChiBanMod> ();
-            //GameObject.DontDestroyOnLoad (temp);
-        }
-        public override void OnUnload()
-        {
-            //GameObject.Destroy (temp);
-        }
-    }
-
-
-    public class wheelMod : MonoBehaviour
+    namespace Blocks
     {
-        public Vector3 Torque;
-        void Start()
-        {        }
-        void Update()
+        public class WheelMod : BlockMod
         {
-            Collider col = this.GetComponentInChildren<Collider>();//Problems are here :/
-            col.material.bounciness = 0.75f;
-            col.material.dynamicFriction = 1;
-            col.material.staticFriction = 1;
-            //col.material.bounceCombine = PhysicMaterialCombine.Maximum;
-            if (AddPiece.isSimulating)
+            public override Version Version { get { return new Version("1.0"); } }
+            public override string Name { get { return "wheelMod"; } }
+            public override string DisplayName { get { return "wheel Mod"; } }
+            public override string BesiegeVersion { get { return "v0.11"; } }
+            public override string Author { get { return "覅是 & tony1020"; } }
+        protected Block wheel = new Block()
+                .ID(501)
+                .TextureFile("whell_map.png")
+                .BlockName("Morden Wheel")
+                .Obj(new List<Obj> { new Obj("wheel.obj", new VisualOffset(Vector3.one, Vector3.zero, Vector3.zero)) })//
+                .Scripts(new Type[] { typeof(wheelMod) })
+                .Properties(new BlockProperties()//.Slider("Speed", 0f, 5f, 1f).Key1("Forward", "8").Key2("backward", "2")*/
+                                                 .ToggleModeEnabled("Freeze me", false)
+                                                 .CanBeDamaged(3)
+                                                 )
+                .Mass(3f)
+                .IconOffset(new Icon(0.2f, new Vector3(-0.1f, -0.1f, 1f), new Vector3(-90f, 45f, 0f)))
+                .ShowCollider(true)
+                .CompoundCollider(new List<ColliderComposite> { new ColliderComposite(new Vector3(5f, 5f, 1.4f)/*大小*/, new Vector3(0f, 0f, 0.7f)/*位置*/, new Vector3(0f, 0f, 0f)/*角度*/),
+            new ColliderComposite(new Vector3(5f, 5f, 1.4f)/*大小*/, new Vector3(0f, 0f, 0.7f)/*位置*/, new Vector3(0f, 0f, 10f)/*角度*/),
+            new ColliderComposite(new Vector3(5f, 5f, 1.4f)/*大小*/, new Vector3(0f, 0f, 0.7f)/*位置*/, new Vector3(0f, 0f, 20f)/*角度*/),
+            new ColliderComposite(new Vector3(5f, 5f, 1.4f)/*大小*/, new Vector3(0f, 0f, 0.7f)/*位置*/, new Vector3(0f, 0f, 30f)/*角度*/),
+            new ColliderComposite(new Vector3(5f, 5f, 1.4f)/*大小*/, new Vector3(0f, 0f, 0.7f)/*位置*/, new Vector3(0f, 0f, 40f)/*角度*/),
+            new ColliderComposite(new Vector3(5f, 5f, 1.4f)/*大小*/, new Vector3(0f, 0f, 0.7f)/*位置*/, new Vector3(0f, 0f, 50f)/*角度*/),
+            new ColliderComposite(new Vector3(5f, 5f, 1.4f)/*大小*/, new Vector3(0f, 0f, 0.7f)/*位置*/, new Vector3(0f, 0f, 60f)/*角度*/),
+            new ColliderComposite(new Vector3(5f, 5f, 1.4f)/*大小*/, new Vector3(0f, 0f, 0.7f)/*位置*/, new Vector3(0f, 0f, 70f)/*角度*/),
+            new ColliderComposite(new Vector3(5f, 5f, 1.4f)/*大小*/, new Vector3(0f, 0f, 0.7f)/*位置*/, new Vector3(0f, 0f, 80f)/*角度*/)})
+                .AddingPoints(new List<AddingPoint> {(AddingPoint)new BasePoint(true, true).Motionable(false, false,false)})
+                .NeededResources(new List<NeededResource>()
+                );
+            public override void OnLoad()
             {
-                if (Input.GetKeyDown(this.GetComponent<MyBlockInfo>().key1)) { Torque = new Vector3(this.GetComponent<MyBlockInfo>().sliderValue, -0.01f /*this.GetComponent<MyBlockInfo>().sliderValue*/, 0f); }
-                if ((Input.GetKeyDown(this.GetComponent<MyBlockInfo>().key2))) { Torque = new Vector3(-this.GetComponent<MyBlockInfo>().sliderValue, 0.01f/* this.GetComponent<MyBlockInfo>().sliderValue*/, 0f); }
-                rigidbody.AddTorque(Torque*1);//This is not working, too
-                Torque = new Vector3(0,0,0);
+                LoadFancyBlock(wheel);
             }
-           
+            public override void OnUnload() { }
+        }
+
+
+        public class wheelMod : BlockScript
+        {
+        public Vector3 Torque;
+        public Collider coll;
+        /*public float diffx;
+        public float diffy;
+        public float diffz;
+        public float sv;*/
+        protected override void OnSimulateStart(){
+            //Still not seems working :/ maybe I need to use meshCollider?
+            Collider col = this.GetComponentInChildren<Collider>();
+            col.material.bounciness = 1;
+            col.material.dynamicFriction = 0;
+            col.material.staticFriction = 0;
+            col.material.bounceCombine = PhysicMaterialCombine.Maximum;
+            col.material.frictionCombine = PhysicMaterialCombine.Minimum;
+            /*sv = 30*this.GetComponent<MyBlockInfo>().sliderValue;*/
+        }
+            
+
+
+        protected override void OnSimulateUpdate()
+        {
             
         }
+        protected override void OnSimulateFixedUpdate()
+        {
+            if (AddPiece.isSimulating)
+            {
+                /*diffz = Vector3.Angle(transform.forward, Vector3.forward);
+                diffx = Vector3.Angle(transform.up, Vector3.up);
+                diffy = Vector3.Angle(transform.right, Vector3.right);
+                Torque = new Vector3(diffx/180 * sv, diffy/180 * sv, diffz/180 * sv);
+                if (Input.GetKey(this.GetComponent<MyBlockInfo>().key1)) { rigidbody.AddTorque(Torque * 1); }
+                if (Input.GetKey(this.GetComponent<MyBlockInfo>().key2)) { rigidbody.AddTorque(Torque * -1); }*/
+                //Physics stuff
+            }
+        }
+
+        protected override void OnSimulateExit()
+        {
+        }
     }
-}
+    }
+    
+
+
+
